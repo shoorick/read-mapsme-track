b145c1f268c809c34778b357f0a0c8ef9a0e9c29
Alexander Sapozhnikov <shoorick@mail.ru>
2017-06-29 18:40:06 +0500

    Main script added

---
+++ read-mapsme-track.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+
+=encoding utf8
+
+=head1 NAME
+
+read-mapsme-track.pl - Read GPS data from Maps with Me (maps.me)
+
+=head1 SYNOPSIS
+
+./read-mapsme-track.pl I<[options]>
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-o>, B<--outtype>
+
+Type of output data. Possible values: csv, tab.
+
+=item B<-?>, B<--help>
+
+Print a brief help message and exits.
+
+=item B<--man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+I<Coming soon>
+
+=head1 AUTHOR
+
+Alexander Sapozhnikov, L<< E<lt>shoorick@cpan.orgE<gt> >>, L<< http://as.susu.ru/ >>
+
+=cut
+
+use Getopt::Long;
+use Pod::Usage qw( pod2usage );
+
+my $CHUNK_SIZE = 65;
+my $HEADER_SIZE = 4;
+
+# TODO Move to config
+my %FORMAT = (
+    'csv' => join(',',  ('%s')x9 ) . "\n",
+    'tab' => join("\t", ('%s')x9 ) . "\n",
+);
+
+my %HEADER = (
+    'csv' => join(',',  qw( timestamp latitude longitude altitude speed bearing horizontal_accuracy vertical_accuracy source ) ) . "\n",
+    'tab' => join("\t", qw( timestamp latitude longitude altitude speed bearing horizontal_accuracy vertical_accuracy source ) ) . "\n",
+);
+
+my %FOOTER = (
+    'csv' => '',
+    'tab' => '',
+);
+
+map { $_ = '' } my ( $need_help, $need_manual, $verbose );
+my $outtype = 'csv';
+
+GetOptions(
+    'outtype=s' => \$outtype,
+    'help|?'    => \$need_help,
+    'manual'    => \$need_manual,
+    'verbose'   => \$verbose,
+);
+
+pod2usage(1)
+    if $need_help;
+pod2usage('verbose' => 2)
+    if $need_manual;
+
+foreach my $file ( @ARGV ) {
+    open my $fh, '<:raw', $file
+        or die "cannot open $file for reading: $!";
+
+    my $bytes_read = read $fh, my $header, $HEADER_SIZE;
+    my $buffer = '';
+    my $offset = $bytes_read;
+
+    print $HEADER{$outtype};
+
+    while ( $bytes_read = read $fh, $buffer, $CHUNK_SIZE ) {
+        my (
+            $timestamp, $latitude, $longitude, $altitude, $speed, $bearing,
+            $horizontal_accuracy, $vertical_accuracy, $source
+        ) = unpack 'ddddddddB', $buffer;
+
+        printf $FORMAT{$outtype},
+            $timestamp, $latitude, $longitude, $altitude, $speed, $bearing,
+            $horizontal_accuracy, $vertical_accuracy, $source;
+    }
+
+    print $FOOTER{$outtype};
+    close $fh
+        or warn "close failed: $!";
+}
